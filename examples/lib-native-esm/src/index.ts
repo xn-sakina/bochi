cb2c493f391e36221c74aa1ae9db97d1ff7ea406
fz6m <59400654+fz6m@users.noreply.github.com>
2024-04-28 05:31:30 +0900

    example: update lib native esm 🍉

--- examples/lib-native-esm/src/index.ts
+++ examples/lib-native-esm/src/index.ts
@@ -1,5 +1,10 @@
 import chalk from 'chalk'
+import { a as a1 } from './other.js'
+import { a as a2 } from './commonjs.cjs'
 
 const a = 1
 console.log(chalk.red(a))
 export const b = a
+
+console.log(a1);
+console.log(a2);
